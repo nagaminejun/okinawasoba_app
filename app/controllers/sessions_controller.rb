f252d3877910d627d15f3de2badeac8de623a1ce
nagaminejun <nagaminejun1988@gmail.com>
2022-09-19 02:00:17 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      # チェックボックスの値を評価します。
+      if params[:session][:remember_me] == "1"
+        remember(user) # オンの時はユーザー情報を記憶します。
+      else
+        forget(user) # オフの場合は記憶しません。redirect_to user
+      end
+      # 10~15は三項演算子にすると下記の１行になる
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user) 構造は以下
+      # [条件式] ? [真（true）の場合実行される処理] : [偽（false）の場合実行される処理]
       flash[:success] = "ログインしました"
       redirect_to user
     else
@@ -15,7 +24,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    # ログイン中の場合のみログアウト処理を実行します。
+    log_out if logged_in?
     flash[:success] = "ログアウトしました"
     redirect_to root_url
   end
